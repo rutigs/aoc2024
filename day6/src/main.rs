32e1b9c34761d4aec1a2640fa33d199fe8e2a1ec
nrutigliano <nrutigliano@spotify.com>
2024-12-07 20:13:33 -0500

    the laziest slowest day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::File, io::{BufRead, BufReader}};
+use std::{collections::{HashMap, HashSet}, fs::File, io::{BufRead, BufReader}};
 
 fn main() {
     let file = File::open("../inputs/day6.txt");
@@ -27,11 +27,11 @@ fn main() {
         row_idx += 1;
      }
 
+    let num_rows = patrol_map.len();
+    let num_cols = patrol_map[0].len();
     // println!("guard starts at {}-{}", guard_locn.0, guard_locn.1);
 
-    println!("Map dims {}x{}", patrol_map.len(), patrol_map[0].len());
-
-    // Part 1
+    // println!("Map dims {}x{}", num_rows, num_cols);
     let dir_vectors: HashMap<char, (i32, i32)> = HashMap::from([
         ('^', (-1, 0)),
         ('<', (0, -1)),
@@ -39,40 +39,104 @@ fn main() {
         ('v', (1, 0))
     ]);
 
-    let mut positions = 1;
-    patrol_map[guard_locn.0 as usize][guard_locn.1 as usize] = 'X';
+    // Part 2
+    let total_squares = num_rows * num_cols;
+    let start_pos = guard_locn;
+    let mut valid_obs = 0;
 
-    while is_on_map(guard_locn, patrol_map.len(), patrol_map[0].len()) {
-        if patrol_map[guard_locn.0 as usize][guard_locn.1 as usize] == '.' {
-            patrol_map[guard_locn.0 as usize][guard_locn.1 as usize] = 'X';
-            positions += 1;
+    for r in 0..num_rows {
+        for c in 0..num_cols {
+            // skip the default guard position
+            if r as i32 == start_pos.0 && c as i32 == start_pos.1 {
+                continue;
+            }
+
+            let mut seen = 1;
+            let mut tmp_map = patrol_map.clone();
+            let mut seen_map: HashSet<(i32, i32, char)> = HashSet::new();
+            seen_map.insert((start_pos.0, start_pos.1, curr_guard));
+            guard_locn = start_pos;
+            tmp_map[r][c] = '#';
+            curr_guard = '^';
+
+            while is_on_map(guard_locn, num_rows, num_cols) && seen < total_squares {
+                let mut peek = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
+                if !is_on_map(peek, num_rows, num_cols) {
+                    break;
+                }
+
+                // println!("curr guard   = {}", curr_guard);
+                // println!("current locn = {},{}", guard_locn.0, guard_locn.1);
+                // println!("current peak = {},{}", peek.0, peek.1);
+
+                while tmp_map[peek.0 as usize][peek.1 as usize] == '#' {
+                    curr_guard = match curr_guard {
+                        '^' => '>',
+                        '<' => '^',
+                        '>' => 'v',
+                        'v' => '<',
+                        _ => panic!("invalid {}", curr_guard),
+                    };
+
+                    peek = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
+                }
+
+                guard_locn = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
+                if seen_map.contains(&(guard_locn.0, guard_locn.1, curr_guard)) {
+                    seen = total_squares;
+                    break;
+                } else {
+                    seen_map.insert((guard_locn.0, guard_locn.1, curr_guard));
+                }
+
+                seen += 1;
+            }
+
+            if seen == total_squares {
+                println!("{},{} <- valid obs", r, c);
+                valid_obs += 1;
+            }
+
         }
-
-        let peek = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
-        if !is_on_map(peek, patrol_map.len(), patrol_map[0].len()) {
-            println!("peek is off map {},{}", peek.0, peek.1);
-            break;
-        }
-
-        println!("curr guard   = {}", curr_guard);
-        println!("current locn = {},{}", guard_locn.0, guard_locn.1);
-        println!("current peak = {},{}", peek.0, peek.1);
-
-        curr_guard = match patrol_map[peek.0 as usize][peek.1 as usize] {
-            '#' => match curr_guard {
-                '^' => '>',
-                '<' => '^',
-                '>' => 'v',
-                'v' => '<',
-                _ => panic!("invalid {}", curr_guard),
-            },
-            _ => curr_guard,
-        };
-
-        guard_locn = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
     }
 
-    println!("{}", positions);
+    println!("{}", valid_obs);
+
+    // Part 1
+    // let mut positions = 1;
+    // patrol_map[guard_locn.0 as usize][guard_locn.1 as usize] = 'X';
+
+    // while is_on_map(guard_locn, num_rows, num_cols) {
+    //     if patrol_map[guard_locn.0 as usize][guard_locn.1 as usize] == '.' {
+    //         patrol_map[guard_locn.0 as usize][guard_locn.1 as usize] = 'X';
+    //         positions += 1;
+    //     }
+
+    //     let peek = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
+    //     if !is_on_map(peek, num_rows, num_cols) {
+    //         println!("peek is off map {},{}", peek.0, peek.1);
+    //         break;
+    //     }
+
+    //     println!("curr guard   = {}", curr_guard);
+    //     println!("current locn = {},{}", guard_locn.0, guard_locn.1);
+    //     println!("current peak = {},{}", peek.0, peek.1);
+
+    //     curr_guard = match patrol_map[peek.0 as usize][peek.1 as usize] {
+    //         '#' => match curr_guard {
+    //             '^' => '>',
+    //             '<' => '^',
+    //             '>' => 'v',
+    //             'v' => '<',
+    //             _ => panic!("invalid {}", curr_guard),
+    //         },
+    //         _ => curr_guard,
+    //     };
+
+    //     guard_locn = (guard_locn.0 + dir_vectors[&curr_guard].0, guard_locn.1 + dir_vectors[&curr_guard].1);
+    // }
+
+    // println!("{}", positions);
 }
 
 fn is_on_map(locn: (i32, i32), rows: usize, cols: usize) -> bool {
